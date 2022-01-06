7e02210a16f4386ee8951a0c268004c95f46320c
ele_11_ven <lwentao@hotmail.com>
2022-01-06 22:25:36 +0800

    export defualt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-export default {
-  evntWrapper: function(ev) {
+export default class {
+  evntWrapper = (ev) => {
     let { pageX, pageY, target } = ev;
     let { left, top } = target.getBoundingClientRect();
     return { x: pageX - left, y: pageY - top };
-  },
-  getOffset: function(ele) {
+  }
+  getOffset = (ele) => {
     let mouse = { x: 0, y: 0 };
     ele.addEventListener('mousemove', function (e) {
       let { x, y } = this.evntWrapper(e);
